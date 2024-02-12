1a6c4bae902475cf690002e9beca855620908ca7
KevMcK <kmcrunk@gmail.com>
2024-02-12 12:17:35 -0600

    learning syntax with leds

---
+++ LED_and_gate.py
@@ -0,0 +1,43 @@
+import itertools
+
+from amaranth import *
+from amaranth.lib import wiring
+from amaranth.lib.wiring import In, Out
+from amaranth.build import ResourceError
+from amaranth_boards.nandland_go import NandlandGoPlatform
+
+class Main(wiring.Component):
+    leds: Out(4)
+    buttons: In(4)
+
+    def elaborate(self, platform):
+        m = Module()
+
+        m.d.comb += self.leds.eq(Cat(self.buttons[0] & self.buttons[1], 0, 0, 0))
+
+        return m
+
+class Top(Elaboratable):
+    def elaborate(self, platform):
+        m = Module()
+
+        def get_all_resources(name):
+            resources = []
+            for number in itertools.count():
+                try:
+                    resources.append(platform.request(name, number))
+                except ResourceError:
+                    break
+            return resources
+
+        m.submodules.blinky = main = Main()
+
+        for li, led in enumerate([res.o for res in get_all_resources("led")]):
+            m.d.comb += led.eq(main.leds[li])
+        for bi, button in enumerate([res.i for res in get_all_resources("button")]):
+            m.d.comb += main.buttons[bi].eq(button)
+
+        return m
+
+if __name__ == "__main__":
+    NandlandGoPlatform().build(Top(), do_program=True, debug_verilog=True)
--- LED_buttons.py
+++ LED_buttons.py
@@ -6,23 +6,21 @@ from amaranth.lib.wiring import In, Out
 from amaranth.build import ResourceError
 from amaranth_boards.nandland_go import NandlandGoPlatform
 
-class Blinky(wiring.Component):
+class Main(wiring.Component):
     leds: Out(4)
     buttons: In(4)
 
     def elaborate(self, platform):
         m = Module()
 
-        clk_freq = platform.default_clk_frequency
-        timer = Signal(range(int(clk_freq//4)), reset=int(clk_freq//4) - 1)
-        flops = Signal(len(self.leds))
+        # All LEDs on
+        # m.d.comb += self.leds.eq(0b1111)
 
-        m.d.comb += self.leds.eq(flops ^ self.buttons)
-        with m.If(timer == 0):
-            m.d.sync += timer.eq(timer.reset)
-            m.d.sync += flops.eq(~flops)
-        with m.Else():
-            m.d.sync += timer.eq(timer - 1)
+        # Button 1 turns on all LEDs
+        # m.d.comb += self.leds.eq(Cat(self.buttons[0], self.buttons[0], self.buttons[0], self.buttons[0]))
+        
+        # Each button turns on related LED
+        m.d.comb += self.leds.eq(Cat(self.buttons[0], self.buttons[1], self.buttons[2], self.buttons[3]))
 
         return m
 
@@ -39,12 +37,12 @@ class Top(Elaboratable):
                     break
             return resources
 
-        m.submodules.blinky = blinky = Blinky()
+        m.submodules.blinky = main = Main()
 
         for li, led in enumerate([res.o for res in get_all_resources("led")]):
-            m.d.comb += led.eq(blinky.leds[li])
+            m.d.comb += led.eq(main.leds[li])
         for bi, button in enumerate([res.i for res in get_all_resources("button")]):
-            m.d.comb += blinky.buttons[bi].eq(button)
+            m.d.comb += main.buttons[bi].eq(button)
 
         return m
 
